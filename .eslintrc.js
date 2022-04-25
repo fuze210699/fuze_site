e68c8caf57602dcdf0d629a45867e13a455909ef
thientn <thientn@midota.com>
2022-04-25 13:23:13 +0700

    ci: implement husk and commitlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'standard'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -13,5 +17,13 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['react', '@typescript-eslint'],
-  rules: {}
-}
+  rules: {
+    'no-unused-vars': [
+      1,
+      {
+        args: 'after-used',
+        argsIgnorePattern: '^_'
+      }
+    ]
+  }
+};
